007143843e0c611bb7ac44f060db88a5c4a3b587
Film <film.tanatchat@gmail.com>
2025-03-08 10:27:09 +0700

    Apply a dark theme for a modern cinematic feel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import MovieDetails from "./components/MovieDetails";
 import Home from "./pages/Home";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: { main: "#90caf9" },
+    secondary: { main: "#f48fb1" },
+    background: { default: "#121212", paper: "#1d1d1d" },
+    text: { primary: "#ffffff" },
+  },
+});
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/movie/:id" element={<MovieDetails />} />
-    </Routes>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
