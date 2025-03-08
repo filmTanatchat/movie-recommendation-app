4b9a47950efffb918393b846462c057af98a5c07
Film <film.tanatchat@gmail.com>
2025-03-08 10:06:38 +0700

    Fix: Add missing searchMovies function

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -6,3 +6,9 @@ export async function fetchTrendingMovies() {
   const data = await response.json();
   return data.results;
 }
+
+export async function searchMovies(query) {
+  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
+  const data = await response.json();
+  return data.results;
+}
