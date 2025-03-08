e3c5376ec77d4c39de82105a4b40cd73376bdf30
Film <film.tanatchat@gmail.com>
2025-03-08 10:00:41 +0700

    Feature: Setup API Key and Fetch Trending Movies

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,14 +2,7 @@ const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export async function fetchTrendingMovies() {
-  console.log("Fetching movies from API..."); // Debugging
-
-  try {
-    const response = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
-    const data = await response.json();
-    console.log("Fetched Data:", data); // ดูว่ามีข้อมูลจริงหรือไม่
-    return data.results;
-  } catch (error) {
-    console.error("Error fetching movies:", error);
-  }
+  const response = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
 }
