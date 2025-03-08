b40052bc563d03a2c01daddc4cdfba7203d402b7
Film <film.tanatchat@gmail.com>
2025-03-08 10:20:08 +0700

    Fix: Remove duplicate BrowserRouter from App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import MovieDetails from "./components/MovieDetails";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<MovieDetails />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<MovieDetails />} />
+    </Routes>
   );
 }
 
