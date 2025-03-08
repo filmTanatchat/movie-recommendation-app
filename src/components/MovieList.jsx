456b96d6a2d399f5653c2aae1cd4d2427d4a7f29
Film <film.tanatchat@gmail.com>
2025-03-08 10:13:27 +0700

    Fix: Remove duplicate SearchBar from MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,31 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { fetchTrendingMovies, searchMovies } from "../services/movieService";
+import React from "react";
 import MovieCard from "./MovieCard";
-import SearchBar from "./SearchBar";
 
-function MovieList() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    async function getMovies() {
-      const moviesData = await fetchTrendingMovies();
-      setMovies(moviesData);
-    }
-    getMovies();
-  }, []);
-
-  const handleSearch = async (query) => {
-    const searchResults = await searchMovies(query);
-    setMovies(searchResults);
-  };
-
+function MovieList({ movies }) {
   return (
     <div className="container">
-      <SearchBar onSearch={handleSearch} />
       <div className="row">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        {movies.length > 0 ? (
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+        ) : (
+          <p className="text-center">No movies found. Try searching for another title.</p>
+        )}
       </div>
     </div>
   );
